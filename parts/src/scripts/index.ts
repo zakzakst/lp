bced09cc44007e461d8deefb8f31d20c89babc53
zakzakst <e1433104@gmail.com>
2021-01-18 21:42:44 +0900

    パーツ：ボタン12

--- parts/src/scripts/index.ts
+++ parts/src/scripts/index.ts
@@ -6,3 +6,6 @@ button04('js-button-04-text');
 import { splitText as button10 } from './components/buttons/button10';
 button10('js-button-04-text');
 button10('js-button-04-hover-text');
+
+import { splitCloneText as button12 } from './components/buttons/button12';
+button12('js-button-12', 'button-12__char');
