033976b6e60871082b36868f0b8dba7696e03576
zakzakst <e1433104@gmail.com>
2021-02-13 07:36:22 +0900

    01：ファイル構成の修正

--- 01/gulpfile.js
+++ 01/gulpfile.js
@@ -5,7 +5,6 @@ const { sassBuild } = require('./tasks/sass-build');
 // const { jsBuild } = require('./tasks/js-build');
 const { imagemin } = require('./tasks/imagemin');
 const { browsersync } = require('./tasks/browsersync');
-// const { webpackDev, webpackPrd } = require('./tasks/webpack');
 const { webpack } = require('./tasks/webpack');
 
 gulp.task('watch-files', (done) => {
@@ -16,6 +15,6 @@ gulp.task('watch-files', (done) => {
 });
 
 gulp.task('imagemin', gulp.series(imagemin));
-// gulp.task('build:dev', gulp.series(nunjucksBuild, sassBuild));
+// gulp.task('build', gulp.series(nunjucksBuild, sassBuild));
 gulp.task('build', gulp.series(nunjucksBuild, sassBuild, webpack));
 gulp.task('default', gulp.series(browsersync.server, 'watch-files'));
