9b69b40e2421954ae1e9f286ca5d097d3b0ee056
zakzakst <e1433104@gmail.com>
2021-02-20 12:52:55 +0900

    02：PC用のスタイル作成

--- 02/src/js/script.ts
+++ 02/src/js/script.ts
@@ -2,6 +2,14 @@
 
 import { VoiceList } from './_module/voice-list';
 
+// ページ表示アニメーション
+(() => {
+  setTimeout(() => {
+    const el = document.querySelector('.hero-section');
+    el.classList.add('is-animated');
+  }, 500);
+})();
+
 // お客様の声スクリプトの実行
 (() => {
   const voiceList = new VoiceList('js-voice-list');
@@ -23,4 +31,18 @@ import { FooterGoTop } from './_module/footer-go-top';
 (() => {
   const footerGoTop = new FooterGoTop('js-footer-go-top');
   footerGoTop.init();
+})();
+
+// 文言分割スクリプトの実行
+import { SplitText } from './_module/split-text';
+(() => {
+  const splitText = new SplitText('js-split-text');
+  splitText.init();
+})();
+
+// スクロールアニメーションスクリプトの実行
+import { ScrollAnim } from './_module/scroll-anim';
+(() => {
+  const scrollAnim = new ScrollAnim('js-scroll-anim');
+  scrollAnim.init();
 })();
