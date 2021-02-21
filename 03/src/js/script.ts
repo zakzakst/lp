0106b1397742f407b803a5f31a387850e14f2382
zakzakst <e1433104@gmail.com>
2021-02-21 18:30:55 +0900

    03：フッターの作成

--- 03/src/js/script.ts
+++ 03/src/js/script.ts
@@ -1,1 +1,8 @@
 'use strict';
+import { FooterGoTop } from './_module/footer-go-top';
+
+// トップへ戻るスクリプトの実行
+(() => {
+  const footerGoTop = new FooterGoTop('js-footer-go-top');
+  footerGoTop.init();
+})();
