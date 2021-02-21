9f99bb3113adbf18584587c40a08a934dc1150bd
zakzakst <e1433104@gmail.com>
2021-02-21 22:14:42 +0900

    03：スクロールアニメーション作成

---
+++ 03/src/js/_module/hero.ts
@@ -0,0 +1,32 @@
+'use strict';
+import $ from 'jquery';
+import 'slick-carousel';
+
+export class Hero {
+  mvEl: JQuery;
+  slickConf: Object;
+  constructor(mvElId: string) {
+    this.mvEl = $(`#${mvElId}`);
+    this.slickConf = {
+      // autoplay: true,
+      autoplay: false,
+      autoplaySpeed: 3000,
+      arrows: false,
+      dots: false,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      fade: true,
+      speed: 2000,
+      swipe: false,
+    };
+  }
+
+  /**
+   * 初期化
+   */
+  init(): void {
+    if (!this.mvEl) return;
+    /* @ts-ignore */
+    this.mvEl.slick(this.slickConf);
+  }
+}
