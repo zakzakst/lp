8407a5ace0535f83e8524b7572f2441a0ff2a3ca
zakzakst <e1433104@gmail.com>
2021-02-28 08:45:09 +0900

    04：ページローダーの作成

--- 04/src/js/_module/loader.ts
+++ 04/src/js/_module/loader.ts
@@ -4,73 +4,20 @@ import { gsap } from "gsap";
 
 export class Loader {
   el: HTMLElement;
-  textEl: HTMLElement;
-  progressEl: HTMLElement;
-  callback: any;
-  imgEls: NodeListOf<HTMLElement>;
-  videoEls: HTMLCollection;
-  targetElNum: number;
-  loadedElNum: number;
-  loadedPercent: number;
-  constructor(
-    elId: string,
-    textElId: string,
-    progressElId: string,
-    callback: any
-  ) {
+  callback;
+  constructor(elId: string, callback: any) {
     this.el = document.getElementById(elId);
-    this.textEl = document.getElementById(textElId);
-    this.progressEl = document.getElementById(progressElId);
     this.callback = callback;
-    // loading属性が「lazy」の画像は読込を監視しない
-    this.imgEls = document.querySelectorAll('img:not([loading="lazy"])');
-    this.videoEls = document.getElementsByTagName('video');
-    this.targetElNum = this.imgEls.length + this.videoEls.length;
-    this.loadedElNum = 0;
-    this.loadedPercent = 0;
-  }
-
-  /**
-   * 初期化
-   */
-  init(): void {
-    this.onLoadImg();
-    this.onLoadVideo();
-    this.onCompleteLoad();
-  }
-
-  /**
-   * ローダーを更新
-   */
-  updateLoader(): void {
-    this.textEl.textContent = `${this.loadedPercent}%`;
-    this.progressEl.style.width = `${this.loadedPercent}%`;
-  }
-
-  /**
-   * 画像・動画読み込み後の更新
-   */
-  elLoadDone(): void {
-    this.loadedElNum += 1;
-    // キャッシュなど画像・動画以外の読込を考慮し、90%までの進捗表示にする（window.onloadで100%にする）
-    this.loadedPercent = Math.floor(this.loadedElNum / this.targetElNum * 90);
-    this.updateLoader();
-  }
-
-  /**
-   * ウインドウ固定を解除
-   */
-  clearWindow(): void {
-    document.documentElement.style.overflow = null;
   }
 
   /**
    * ローダーを閉じる
    */
-  closeLoader(): void {
+  closeLoader() {
     if (!this.el) return;
     const self = this;
     const tl = gsap.timeline();
+    // TODO: オープンアニメーションを追加
     this.clearWindow();
     tl
       .to(this.el, {
@@ -85,38 +32,9 @@ export class Loader {
   }
 
   /**
-   * 画像読み込み後のイベント
+   * ウインドウ固定を解除
    */
-  onLoadImg(): void {
-    [...this.imgEls].forEach(el => {
-      el.addEventListener('load', () => {
-        this.elLoadDone();
-      });
-    });
-  }
-
-  /**
-   * 動画読み込み後のイベント
-   */
-  onLoadVideo(): void {
-    [...this.videoEls].forEach(el => {
-      el.addEventListener('loadeddata', () => {
-        this.elLoadDone();
-      });
-    });
-  }
-
-  /**
-   * ロード完了時のイベント
-   */
-  onCompleteLoad(): void {
-    window.onload = () => {
-      this.loadedElNum = this.targetElNum;
-      this.loadedPercent = 100;
-      this.updateLoader();
-      setTimeout(() => {
-        this.closeLoader();
-      }, 500);
-    };
+  clearWindow() {
+    document.documentElement.style.overflow = null;
   }
 }
--- 04/src/js/script.ts
+++ 04/src/js/script.ts
@@ -3,8 +3,23 @@
 import $ from 'jquery';
 import 'lightbox2';
 
+import { Loader } from './_module/loader';
 import { FooterGoTop } from './_module/footer-go-top';
 import { AnchorScroll } from './_module/anchor-scroll';
+
+// ページ表示アニメーション
+(() => {
+  const loader = new Loader('js-loader', () => {
+    // TODO: オープンアニメーションを追加
+    // ローダーを閉じた後の挙動を記載
+  });
+  // データ読み込み完了後、ローダーを閉じる
+  window.onload = () => {
+    setTimeout(() => {
+      loader.closeLoader();
+    }, 400);
+  }
+})();
 
 // トップへ戻るスクリプトの実行
 (() => {
